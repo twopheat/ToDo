2ca6b51927e88331df8730a1fd8d05b2c3265e23
Brad Johnston <twopheat@gmail.com>
2019-11-09 13:52:26 -0800

    Final Solved commit

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function renderTodos() {
 
     var li = document.createElement("li");
     li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);
     todoList.appendChild(li);
   }
 }
@@ -39,4 +45,19 @@ todoForm.addEventListener("submit", function(event) {
 
   // Re-render the list
   renderTodos();
+});
+
+// When a element inside of the todoList is clicked...
+todoList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // If that element is a button...
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    todos.splice(index, 1);
+
+    // Re-render the list
+    renderTodos();
+  }
 });
